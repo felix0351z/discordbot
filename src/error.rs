ca30def5547433471430503430fa274454f4efa2
Felix <felix0351z@gmail.com>
2025-03-14 18:42:10 +0100

    Added respond message if lavalink can't play a track. And refactored error handler

--- src/error.rs
+++ src/error.rs
@@ -7,27 +7,17 @@ use crate::music::MusicCommandError;
 type PoiseContext<'a> = Option<Context<'a, Data, Error>>;
 
 pub async fn error_handler(framework_err: FrameworkError<'_, Data, Error>) {
+    // get the poise context to interact with the text channel later.
     let ctx = framework_err.ctx();
 
     match framework_err {
-        FrameworkError::Setup { .. } => {}
+        FrameworkError::Setup { ref error, .. } => { log::error!("Initial setup: {}", error); }
         FrameworkError::Command { ref error, .. } => {
-            // Check for specific error
-            if let Some(lavalink_error) = error.downcast_ref::<LavalinkError>() {
-                match lavalink_error {
-                    LavalinkError::HyperClientError(..) => {
-                        log_error(ctx, "No connection to lavalink server!", lavalink_error).await;
-                    }
-                    &_ => { log::error!("Lavalink: {}", error); }
-                }
-            }
-            else if let Some(music_error) = error.downcast_ref::<MusicCommandError>() {
-                // Send message to channel if a music error occurs
-                // No log in console because it is no real error
-                if let Some(ctx) = ctx {
-                    let _ = ctx.send(create_embed(music_error.to_string())).await;
-                }
-            }
+            // Handle all known errors
+            if let Some(lavalink_error) = error.downcast_ref::<LavalinkError>() { handle_lavalink_error(ctx, lavalink_error).await; }
+            else if let Some(music_error) = error.downcast_ref::<MusicCommandError>() { handle_music_error(ctx, music_error).await; }
+
+            // If the error is not known yet, log it to the console
             else {
                 log::error!("Command execution: {}", error);
             }
@@ -37,6 +27,32 @@ pub async fn error_handler(framework_err: FrameworkError<'_, Data, Error>) {
 }
 
 
+/// Handle a thrown lavalink error
+async fn handle_lavalink_error(ctx: PoiseContext<'_>, error: &LavalinkError) {
+    match error {
+        LavalinkError::HyperClientError(..) => {
+            // Notify that no successfully connection was established yet
+            log_error(ctx, "No connection to lavalink server!", error).await;
+        }
+        LavalinkError::TrackError(..) => {
+            // Notify that lavalink has a problem with the current track
+            log_error(ctx, "Lavalink:", error).await;
+        }
+        // If the error is not known yet, log it to the console
+        &_ => { log::error!("Lavalink: {}", error); }
+    }
+}
+
+/// Handle a thrown music error
+async fn handle_music_error(ctx: PoiseContext<'_>, error: &MusicCommandError) {
+    // Don't log the error in the console, because it is a user error only
+    if let Some(ctx) = ctx {
+        let _ = ctx.send(create_embed(error.to_string())).await;
+    }
+}
+
+
+/// Log the error in the console and notify the user in the guild channel, if a request or was made.
 async fn log_error<T: std::error::Error>(ctx: PoiseContext<'_>, msg: &str, err: T) {
     // Log message in console
     log::error!("{msg}: {err}");
@@ -52,6 +68,7 @@ async fn log_error<T: std::error::Error>(ctx: PoiseContext<'_>, msg: &str, err: 
     }
 }
 
+/// Create a CreateReply object from a text string
 fn create_embed(msg: impl Into<String>) -> CreateReply {
     let creator = CreateEmbed::new()
         .description(msg)
